55f5939043dd51dfdbc3770a9cbc280bdf3dfdf6
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2025-04-05 20:00:23 +0500

    оптимизировал получение популярных тегов

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,6 @@ from blog.models import Comment, Post, Tag
 from django.db.models import Count
 
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
-
-
 def serialize_post(post):
     return {
         'title': post.title,
@@ -24,24 +20,17 @@ def serialize_post(post):
 
 def serialize_tag(tag):
     return {
-        'title': tag.title,
+        'title': tag.title, 
         'posts_with_tag': len(Post.objects.filter(tags=tag)),
-    }
-
-
-def get_likes_count(post):
-    return post.likes_count
+        }
 
 
 def index(request):
     most_popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
-
-    tags = Tag.objects.all()
-    popular_tags = sorted(tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
-
+    most_popular_tags = Tag.objects.annotate(related_posts_count=(Count('posts'))).order_by('-related_posts_count')[:5]
+    
     context = {
         'most_popular_posts': [
             serialize_post(post) for post in most_popular_posts
