eb5ab94252a737ad002dc268b38e0dc2e79389ef
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2025-05-10 19:46:41 +0500

    изменил реверс списка

--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,7 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.popular()][::-1],
+        'tags': reversed(list(serialize_tag(tag) for tag in post.tags.popular())),
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -89,7 +89,7 @@ def post_detail(request, slug):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.popular()][::-1],
+        'tags': reversed(list(serialize_tag(tag) for tag in post.tags.popular())),
     }
 
     most_popular_tags = Tag.objects.popular()[:5]
