567d26dcc3c960dcb146f3781f0297d7e51eec7a
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2025-05-11 16:09:23 +0500

    Починил админ панель и убрал долгую загрузку

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from blog.models import Post, Tag, Comment
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_absolute_url', 'text', 'image', 'published_at', 'author')
+    raw_id_fields = ("likes", 'tags')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text', 'published_at')
+    raw_id_fields = ("author", 'post')
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
